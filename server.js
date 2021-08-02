d352e2dddc23f593dfb95b0e18d2095181367973
Asadullah Khan <asadmuhammad427@gmail.com>
2021-08-02 15:14:20 +0500

    edit server file

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ app.get("/uploads/:id", (req, res) => {
 app.post("/api/metaData/", async (req, res) => {
 try {
   let data = req.body
-  console.log(`data ------------>`, data)
+  console.log(`data ------------>`, req.body.id)
   database.insert(data)
-  res.json({msg:"success",url:`${HOST}/getMetaData/${data.id}`})
+  res.json({msg:"success",url:`${HOST}/getMetaData/${req.body.id}`})
 } catch (error) {
   res.json(error)
 }
